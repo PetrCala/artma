6c5d1c1c994c2d0c222062ca75c394fb81795c7e
PetrCala <cala.p@seznam.cz>
2024-01-24 15:21:42 +0100

    Added a bunch of FE components to work with later

--- React/client/src/pages/api/auth/[...nextauth].ts
+++ React/client/src/pages/api/auth/[...nextauth].ts
@@ -1,26 +1,67 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import PostgresAdapter from "@auth/pg-adapter"
+import { Pool } from "pg"
+
+const pool = new Pool({
+  host: "localhost",
+  user: "database-user",
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+})
 
 const authOptions: NextAuthOptions = {
+  adapter: PostgresAdapter(pool) as any,
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     // ...add more providers here
   ],
+  // pages: {
+  //   signIn: "/auth/signin",
+  //   signOut: "/auth/signout",
+  //   error: "/auth/error", // Error code passed in query string as ?error=
+  //   verifyRequest: "/auth/verify-request", // (used for check email message)
+  //   newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  // },
   callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true
+      if (isAllowedToSignIn) {
+        return true
+      } else {
+        // Return false to display a default error message
+        return false
+        // Or you can return a URL to redirect to:
+        // return '/unauthorized'
       }
-      return token;
+    },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
     },
     async session({ session, token, user }) {
-      // session.accessToken = token.accessToken;
-      return session; // The return type will match the one returned in `useSession()`
+      // Send properties to the client, like an access_token and user id from a provider.
+      // session.accessToken = token.accessToken
+      // session.user.id = token.id
+
+      return session
+    },
+    async jwt({ token, account, profile }) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+        // token.id = profile.id
+      }
+      return token
     },
   },
-};
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
