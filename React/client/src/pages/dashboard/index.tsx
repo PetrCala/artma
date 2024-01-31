b560796e68b416510e19d2e88a1629267147dc6a
PetrCala <cala.p@seznam.cz>
2024-01-31 10:11:36 +0100

    Added a simple dashboards page

---
+++ React/client/src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react"
+
+const Dashboards: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard page!</p>
+    </div>
+  )
+}
+
+export default Dashboards
