db37991d8461c308d02718103f85d6126b12d9d2
PetrCala <cala.p@seznam.cz>
2024-02-07 12:18:24 +0100

    Cleaned up references, object names

--- React/client/src/pages/dashboard/index.tsx
+++ React/client/src/pages/dashboard/index.tsx
@@ -4,7 +4,7 @@ import CONST from "@/CONST"
 
 const Dashboard: React.FC = (): ReactElement => {
   return (
-    <div className={styles.commonPageContainer}>
+    <div className="commonPageContainer">
       <h3>{CONST.APP_NAME_SHORT}</h3>
       <p>{CONST.APP_MOTTO}</p>
       <p className={styles.themeTest}>hello</p>
