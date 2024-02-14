28d901982b10280ece242af1986f7383464e3191
PetrCala <cala.p@seznam.cz>
2024-02-14 10:00:58 +0100

    Minor changes

--- React/client/src/ROUTES.ts
+++ React/client/src/ROUTES.ts
@@ -1,39 +1,47 @@
-import type {IsEqual, ValueOf} from 'type-fest';
+import type { IsEqual, ValueOf } from "type-fest"
 
 const ROUTES = {
-    HOME: '/dashboard',
-    ABOUT: '/about',
-    HELP: '/help',
-    DATA: '/data',
-    DATA_UPLOAD: '/data/upload',
-    DATA_UPLOAD_CSV: '/data/upload/csv',
-    DATA_UPLOAD_JSON: '/data/upload/json',
-    DATA_UPLOAD_XML: '/data/upload/xml',
-    DATA_UPLOAD_XLSX: '/data/upload/xlsx',
-    DATA_UPLOAD_ZIP: '/data/upload/zip',
-    DATA_UPLOAD_ZIP_CSV: '/data/upload/zip/csv',
-    DATA_UPLOAD_ZIP_JSON: '/data/upload/zip/json',
-    DATA_UPLOAD_ZIP_XML: '/data/upload/zip/xml',
-    DATA_UPLOAD_ZIP_XLSX: '/data/upload/zip/xlsx',
-    DATA_UPLOAD_ZIP_XLS: '/data/upload/zip/xls',
-    DATA_UPLOAD_ZIP_XLSM: '/data/upload/zip/xlsm',
-    DATA_UPLOAD_ZIP_ODS: '/data/upload/zip/ods',
-    DATA_UPLOAD_ZIP_ODT: '/data/upload/zip/odt',
-    DATA_VIEW: '/data/view',
-    RESULTS: '/results',
-    SETTINGS: '/settings',
-} as const;
+  HOME: "/dashboard",
+  ABOUT: "/about",
+  HELP: "/help",
+  DATA: "/data",
+  DATA_UPLOAD: "/data/upload",
+  DATA_UPLOAD_CSV: "/data/upload/csv",
+  DATA_UPLOAD_JSON: "/data/upload/json",
+  DATA_UPLOAD_XML: "/data/upload/xml",
+  DATA_UPLOAD_XLSX: "/data/upload/xlsx",
+  DATA_UPLOAD_ZIP: "/data/upload/zip",
+  DATA_UPLOAD_ZIP_CSV: "/data/upload/zip/csv",
+  DATA_UPLOAD_ZIP_JSON: "/data/upload/zip/json",
+  DATA_UPLOAD_ZIP_XML: "/data/upload/zip/xml",
+  DATA_UPLOAD_ZIP_XLSX: "/data/upload/zip/xlsx",
+  DATA_UPLOAD_ZIP_XLS: "/data/upload/zip/xls",
+  DATA_UPLOAD_ZIP_XLSM: "/data/upload/zip/xlsm",
+  DATA_UPLOAD_ZIP_ODS: "/data/upload/zip/ods",
+  DATA_UPLOAD_ZIP_ODT: "/data/upload/zip/odt",
+  DATA_VIEW: "/data/view",
+  USER_VIEW: {
+    route: "/user/:uid/view",
+    getRoute: (uid: string) => `/user/${uid}/view`,
+  },
+  RESULTS: "/results",
+  SETTINGS: "/settings",
+} as const
 
-export default ROUTES;
+export default ROUTES
 
-type ExtractRouteName<TRoute> = TRoute extends {getRoute: (...args: any[]) => infer TRouteName} ? TRouteName : TRoute;
+type ExtractRouteName<TRoute> = TRoute extends {
+  getRoute: (...args: any[]) => infer TRouteName
+}
+  ? TRouteName
+  : TRoute
 
 type AllRoutes = {
-    [K in keyof typeof ROUTES]: ExtractRouteName<(typeof ROUTES)[K]>;
-}[keyof typeof ROUTES];
+  [K in keyof typeof ROUTES]: ExtractRouteName<(typeof ROUTES)[K]>
+}[keyof typeof ROUTES]
 
-type RouteIsPlainString = IsEqual<AllRoutes, string>;
+type RouteIsPlainString = IsEqual<AllRoutes, string>
 
-type Route = RouteIsPlainString extends true ? never : AllRoutes;
+type Route = RouteIsPlainString extends true ? never : AllRoutes
 
-export type {Route};
+export type { Route }
