587a9cffef66fe5f3daeca7b3e09c9a08a7d5458
PetrCala <cala.p@seznam.cz>
2024-12-11 14:10:26 +0100

    Update the box imports so that they work out of the box

--- r/artma.R
+++ r/artma.R
@@ -1,16 +1,14 @@
+# source("R/utils/options.R")
+
 #' @export
 artma <- function() {
-  # load_options()
+  box::use(
+    modules / options[load_options]
+  )
 
 
-  parser <- optparse::OptionParser()
-  parser <- optparse::add_option(parser, c("-v", "--verbose"), action = "store_true", default = TRUE, help = "Print extra output [default]")
-  parser <- optparse::add_option(parser, c("-q", "--quietly"), action = "store_false", dest = "verbose", help = "Print little output")
-  parser <- optparse::add_option(parser, c("-c", "--count"), type = "integer", default = 5, help = "Number of random normals to generate [default %default]", metavar = "number")
-  args <- optparse::parse_args(parser, args = c("--quietly", "--count=15"))
 
-  options(artma = args)
+  load_options()
   # Usage
-  print("hello, world!")
-  # print(getOption("artma"))
+  print(getOption("artma"))
 }
