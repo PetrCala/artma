2e09a0b5af0ed7de95f7a1f003e3942b6a3ac356
PetrCala <cala.p@seznam.cz>
2024-12-11 15:42:19 +0100

    Revert several changes

--- r/artma.R
+++ r/artma.R
@@ -1,18 +1,16 @@
 #' @export
 artma <- function(config_file = NULL, args = commandArgs(trailingOnly = TRUE)) {
-  print(box::file())
-  #   box::use(
-  #     artma / options[load_options],
-  #     artma / const[CONST]
-  #   )
+  box::use(
+    artma / options[load_options],
+    artma / const[CONST]
+  )
 
-  #   config_file <- config_file %||% glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}")
+  config_file <- config_file %||% glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}")
 
-  #   # load_options(glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}"), args = args)
-  #   # load_options(glue::glue("abc.{CONST$OPTIONS_FILE_NAME}"))
-  #   # print(glue::glue("abc.{CONST$OPTIONS_FILE_NAME}"))
-  #   # Usage
-  #   print(config_file)
-  #   # print(getOption("artma"))
-  # }
+  # load_options(glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}"), args = args)
+  # load_options(glue::glue("abc.{CONST$OPTIONS_FILE_NAME}"))
+  # print(glue::glue("abc.{CONST$OPTIONS_FILE_NAME}"))
+  # Usage
+  print(config_file)
+  # print(getOption("artma"))
 }
