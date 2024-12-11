8a8c126615f18babc2b6d3e6912b3d8c8a0ac335
PetrCala <cala.p@seznam.cz>
2024-12-11 15:26:11 +0100

    Move the modules into the R folder

--- r/artma.R
+++ r/artma.R
@@ -1,8 +1,8 @@
 #' @export
 artma <- function(config_file = NULL, args = commandArgs(trailingOnly = TRUE)) {
   box::use(
-    mod / options[load_options],
-    mod / const[CONST]
+    . / src / options[load_options],
+    . / src / const[CONST]
   )
 
   config_file <- config_file %||% glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}")
