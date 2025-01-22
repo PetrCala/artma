94e8a37fcfbbb50279a1d61967a49f55425ca437
PetrCala <cala.p@seznam.cz>
2025-01-22 15:22:45 +0100

    Add a path to the project root

--- inst/artma/const.R
+++ inst/artma/const.R
@@ -1,7 +1,8 @@
 PACKAGE_NAME <- "artma"
 PACKAGE_NAME_VERBOSE <- "Automatic Replication Tools for Meta-Analysis"
 
-#' @returns The path to the folder where the package inst main modules (inst)are installed
+#' @returns The path to the folder where the main 'artma' folder is located. In case the package is installed, the path to the package folder is returned. In case the package is in development mode, the path to the 'inst' folder is returned instead.
+#' @export
 get_pkg_path <- function() {
   box_path <- getOption("box.path")
   dev_path <- box_path[grepl(glue::glue("{PACKAGE_NAME}/inst$"), box_path)]
@@ -14,6 +15,7 @@ get_pkg_path <- function() {
 }
 
 PACKAGE_PATH <- get_pkg_path()
+PROJECT_ROOT <- glue::glue("{PACKAGE_PATH}/{PACKAGE_NAME}")
 
 #' @export
 CONST <- list(
@@ -21,7 +23,8 @@ CONST <- list(
   PACKAGE_NAME = PACKAGE_NAME,
   PACKAGE_NAME_VERBOSE = PACKAGE_NAME_VERBOSE,
   PACKAGE_PATH = PACKAGE_PATH,
+  PROJECT_ROOT = PROJECT_ROOT, # Where the package modules are located
 
   # File names
-  OPTIONS_FILE_NAME = "options.yaml"
+  OPTIONS_FILE_NAME = glue::glue("{PROJECT_ROOT}/options.yaml")
 )
