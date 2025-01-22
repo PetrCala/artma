6894aad8e077f35c372fcc4eace535ac7ce36f67
PetrCala <cala.p@seznam.cz>
2025-01-22 14:13:27 +0100

    Experimental changes

--- r/artma.R
+++ r/artma.R
@@ -1,4 +1,16 @@
-source(file.path(dirname(normalizePath(sys.frame(1)$ofile)), "config.R"))
+# source(file.path(dirname(normalizePath(sys.frame(1)$ofile)), "config.R"))
+
+current_box_path <- getOption("box.path", character(0))
+
+new_path <- paste(find.package("artma"), "R", sep = "/") # ../site-library/artma/R/
+
+if (!any(grepl("artma/R$", current_box_path))) {
+  # Make the package available to the box
+  options(
+    box.path = c(current_box_path, new_path)
+  )
+}
+
 
 #' @export
 artma <- function(config_file = NULL, args = commandArgs(trailingOnly = TRUE)) {
--- r/config.R
+++ r/config.R
@@ -1,7 +1,10 @@
-options(
+current_box_path <- getOption("box.path", character(0))
+
+new_path <- paste(find.package("artma"), "R", sep = "/") # ../site-library/artma/R/
+
+if (!any(grepl("artma/R$", current_box_path))) {
   # Make the package available to the box
-  box.path = c(
-    getOption("box.path"),
-    paste(find.package("artma"), "R", sep = "/") # ../site-library/artma/R/
+  options(
+    box.path = c(current_box_path, new_path)
   )
-)
+}
