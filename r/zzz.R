84111dff12117508f2b4edf65a62c30062319c0c
PetrCala <cala.p@seznam.cz>
2025-01-22 15:04:19 +0100

    The boxpath check function now adds a development path too

--- r/zzz.R
+++ r/zzz.R
@@ -7,9 +7,10 @@
 ensure_valid_boxpath <- function() {
   current_box_path <- getOption("box.path", character(0))
   pkg_box_path <- find.package("artma")
+  dev_box_path <- paste(pkg_box_path, "inst", sep = "/") # For local development
 
   if (!any(grepl("artma$", current_box_path))) {
     # Make the package available to the box options(
-    options(box.path = c(current_box_path, pkg_box_path))
+    options(box.path = c(current_box_path, pkg_box_path, dev_box_path))
   }
 }
