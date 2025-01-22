ed184bb32945f479f599be1d3939da7a56bc90e6
PetrCala <cala.p@seznam.cz>
2025-01-22 13:30:55 +0100

    Experimental changes

--- r/artma.R
+++ r/artma.R
@@ -1,16 +1,17 @@
 #' @export
 artma <- function(config_file = NULL, args = commandArgs(trailingOnly = TRUE)) {
-  box::use(
-    R / artma / options[load_options],
-    R / artma / const[CONST]
-  )
+  # box::use(
+  #   R / artma / options[load_options],
+  #   R / artma / const[CONST]
+  # )
 
-  config_file <- config_file %||% glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}")
+  # config_file <- config_file %||% glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}")
 
   # load_options(glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}"), args = args)
   # load_options(glue::glue("abc.{CONST$OPTIONS_FILE_NAME}"))
   # print(glue::glue("abc.{CONST$OPTIONS_FILE_NAME}"))
   # Usage
-  print(config_file)
+  print("Hello, world!")
+  # print(config_file)
   # print(getOption("artma"))
 }
