5991b1e0c60b09c9cf90e6282840f073c4c75a3e
PetrCala <cala.p@seznam.cz>
2025-01-22 12:55:20 +0100

    Further import changes

--- r/artma.R
+++ r/artma.R
@@ -1,8 +1,8 @@
 #' @export
 artma <- function(config_file = NULL, args = commandArgs(trailingOnly = TRUE)) {
   box::use(
-    artma / artma / options[load_options],
-    artma / artma / const[CONST]
+    R / artma / options[load_options],
+    R / artma / const[CONST]
   )
 
   config_file <- config_file %||% glue::glue("modules/static/{CONST$OPTIONS_FILE_NAME}")
