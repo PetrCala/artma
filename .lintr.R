209b28bbdd3c28cf07caf3db1f2bde8430864da0
PetrCala <cala.p@seznam.cz>
2025-01-29 16:21:53 +0100

    Modify box export lint rules

--- .lintr.R
+++ .lintr.R
@@ -1,34 +1,42 @@
-# nolint start: indentation_linter, undesirable_function_linter.
+# nolint start: indentation_linter, undesirable_function_linter, unused_declared_object.
 
 custom_linters_env <- new.env()
 
 source(file.path("R", "linters.R"), local = custom_linters_env)
 
-linters <- lintr::all_linters(
-    # https://lintr.r-lib.org/reference/index.html#individual-linters
-    #
-    # Set indentation to 8 spaces
-    indentation_linter = lintr::indentation_linter(2L),
-    # Check that all commas are followed by spaces, but do not have spaces before them.
-    commas_linter = lintr::commas_linter(allow_trailing = FALSE),
-    # Check that all comments are preceded by a space
-    object_name_linter = lintr::object_name_linter(
-        styles = c("snake_case", "SNAKE_CASE", "dotted.case")
+linters <- c(
+    lintr::all_linters(
+        # https://lintr.r-lib.org/reference/index.html#individual-linters
+        #
+        # Set indentation to 8 spaces
+        indentation_linter = lintr::indentation_linter(2L),
+        # Check that all commas are followed by spaces, but do not have spaces before them.
+        commas_linter = lintr::commas_linter(allow_trailing = FALSE),
+        # Check that all comments are preceded by a space
+        object_name_linter = lintr::object_name_linter(
+            styles = c("snake_case", "SNAKE_CASE", "dotted.case")
+        ),
+        # Disable the default lintr object usage - replace with box imports
+        object_usage_linter = NULL,
+        # All lines should be less than 120 characters
+        line_length_linter = lintr::line_length_linter(NULL), # 120L, 160L,...
+        # Disable commented code linter
+        commented_code_linter = NULL,
+        # Disable the forbidden access through $ linter
+        extraction_operator_linter = NULL,
+        # Disable cyclocompexity linter
+        cyclocomp_linter = NULL
     ),
-    line_length_linter = lintr::line_length_linter(120L),
-    # Disable commented code linter
-    commented_code_linter = NULL,
-    # Disable the forbidden access through $ linter
-    extraction_operator_linter = NULL,
-    # Disable cyclocompexity linter
-    cyclocomp_linter = NULL,
-    ### Custom linters
+    # All default box linters
+    box.linters::box_default_linters,
+    # Custom linters
     # Disable the usage of 'dir.create' in favor of 'fs::dir_create'
     dir_create_linter = custom_linters_env$dir_create_linter()
 )
 
 exclusions <- list(
-    "tests/"
+    "tests/",
+    "archive/"
 )
 
 # Clean up imported linters
@@ -36,4 +44,4 @@ rm(list = ls(custom_linters_env), envir = custom_linters_env)
 rm(custom_linters_env)
 gc() # Clean up memory
 
-# nolint end: indentation_linter, undesirable_function_linter.
+# nolint end.
