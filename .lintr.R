367337fcc480fbec7a6e1c911b3f36bac73ea335
PetrCala <cala.p@seznam.cz>
2025-01-29 14:45:08 +0100

    Add several disabled linters, change the line length to 120

--- .lintr.R
+++ .lintr.R
@@ -1,4 +1,6 @@
-source(file.path("scripts", "src", "linters.R"), local = TRUE) # nolint
+# nolint start: indentation_linter
+
+source(file.path("R", "linters.R"), local = TRUE) # nolint
 
 linters <- lintr::all_linters(
     # https://lintr.r-lib.org/reference/index.html#individual-linters # nolint
@@ -8,18 +10,16 @@ linters <- lintr::all_linters(
     # Check that all commas are followed by spaces, but do not have spaces before them.
     commas_linter = lintr::commas_linter(allow_trailing = FALSE),
     # Check that all comments are preceded by a space
-    object_name_linter = lintr::object_name_linter(styles = c("snake_case", "SNAKE_CASE", "dotted.case")),
-    # Disable the object usage linter
-    object_usage_linter = NULL,
-    # Disable line length limiters
-    line_length_linter = NULL,
-    # Disable assignment linter if you use assignment arrows other than `<-`
-    assignment_linter = NULL,
+    object_name_linter = lintr::object_name_linter(
+        styles = c("snake_case", "SNAKE_CASE", "dotted.case")
+    ),
+    line_length_linter = lintr::line_length_linter(120L),
     # Disable commented code linter
     commented_code_linter = NULL,
     # Disable cyclocompexity linter
     cyclocomp_linter = NULL,
-    pipe_call_linter = pipe_call_linter(),
+    ### Custom linters
+    # Disable the usage of 'dir.create' in favor of 'fs::dir_create'
     dir_create_linter = dir_create_linter()
 )
 ### Custom linters
@@ -29,3 +29,5 @@ linters <- lintr::all_linters(
 exclusions <- list(
     "tests/" # nolint
 )
+
+# nolint end: indentation_linter
