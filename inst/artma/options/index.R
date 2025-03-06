d45bbc3a3780921eecb310af9165b3d6a5e52e55
PetrCala <cala.p@seznam.cz>
2025-03-06 16:12:47 +0100

    Add a missing import

--- inst/artma/options/index.R
+++ inst/artma/options/index.R
@@ -1,5 +1,8 @@
 box::use(
-  artma / options / template[parse_options_from_template],
+  artma / options / template[
+    flatten_template_options,
+    parse_options_from_template
+  ],
   artma / options / utils[list_user_options_files],
   artma / options / public[
     copy_user_options_file,
@@ -14,6 +17,7 @@ box::export(
   copy_user_options_file,
   delete_user_options_file,
   create_user_options_file,
+  flatten_template_options,
   list_user_options_files,
   load_user_options,
   parse_options_from_template,
