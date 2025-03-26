0e29c9f8505b5880a24eb573336c8a98a3ccdd37
PetrCala <cala.p@seznam.cz>
2025-03-26 16:44:00 +0100

    Remove a redundant R scripts folder

--- inst/artma/scripts/quit_r.R
+++
@@ -1,8 +0,0 @@
-#' Quit R from the interactive browser
-#' @export
-quit_debugger_and_console <- function() {
-  if (interactive()) {
-    cat("Q\n") # Quit debugger
-    cat("q()\n") # Quit R console
-  }
-}
