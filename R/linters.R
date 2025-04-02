728659e616dae4fecee64bf8ebb982ee33471654
PetrCala <cala.p@seznam.cz>
2025-04-02 14:35:47 +0200

    Remove the @seealso in the linters file

--- R/linters.R
+++ R/linters.R
@@ -25,7 +25,6 @@
 #'   linters = dir_create_linter()
 #' )
 #' }
-#' @seealso [lintr::linters] for a complete list of linters available in lintr.
 #' @importFrom lintr make_linter_from_xpath
 #' @keywords internal
 dir_create_linter <- lintr::make_linter_from_xpath(
