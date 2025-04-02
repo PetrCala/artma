f390f0aa5716a17c2190a38c6f80db84274acfde
PetrCala <cala.p@seznam.cz>
2025-04-02 13:24:01 +0200

    Remove the object usage linter suppression in the dummy functions module

--- R/dummy_functions.R
+++ R/dummy_functions.R
@@ -1,9 +1,9 @@
 # This script serves as a placeholder for the functions that are used inside the internal 'inst/artma' folder logic, so that the R compiler can treat them as 'used' and not throw warnings about unused functions.
 
-# nolint start: object_usage_linter, box_usage_linter, unused_declared_object_linter.
+# nolint start: box_usage_linter, unused_declared_object_linter.
 
 #' @keywords internal
 .dummy_use_stringr <- function() stringr::str_trim("")
 
 
-# nolint end: object_usage_linter, box_usage_linter, unused_declared_object_linter.
+# nolint end: box_usage_linter, unused_declared_object_linter.
