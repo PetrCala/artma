16741b960b7a0d25507348ff6fba306c452f3bc3
PetrCala <cala.p@seznam.cz>
2025-04-05 15:15:40 +0200

    Modify the release tools to automatically submit to CRAN with different checks

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,19 @@ set -e
 
 . "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
+NO_BOX_CHECK=false
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+    --no-box-check) NO_BOX_CHECK=true ;;
+    *)
+        error "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
 if ! command -v R &>/dev/null; then
     error "R is not installed. Make sure to install it from https://www.cran.r-project.org."
     exit 1
@@ -13,19 +26,16 @@ info "Setting up the environment..."
 
 cd $PROJECT_ROOT
 
-if [[ ! -d $CACHE_DIR ]]; then
-    info "Creating cache directory..."
-    mkdir $CACHE_DIR
-fi
-
 source "$SCRIPTS_DIR/setupR.sh" # Prints verbose output
 
 # Validate box.path
-if ! grep -q "box.path" ~/.Rprofile; then
-    BOX_PATH_ERR_MSG="box.path is not set in ~/.Rprofile. Please add the following line to your ~/.Rprofile file:"
-    BOX_PATH_ERR_MSG+="options(box.path = \"$PROJECT_ROOT/inst\")"
-    error "$BOX_PATH_ERR_MSG"
-    exit 1
+if [[ $NO_BOX_CHECK == false ]]; then
+    if ! grep -q "box.path" ~/.Rprofile; then
+        BOX_PATH_ERR_MSG="box.path is not set in ~/.Rprofile. Please add the following line to your ~/.Rprofile file:"
+        BOX_PATH_ERR_MSG+="options(box.path = \"$PROJECT_ROOT/inst\")"
+        error "$BOX_PATH_ERR_MSG"
+        exit 1
+    fi
 fi
 
 success "Done."
