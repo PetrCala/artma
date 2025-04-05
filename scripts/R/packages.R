a9f6f31bb10b410d622a5287dada80673fb4c16c
PetrCala <cala.p@seznam.cz>
2025-04-05 14:59:39 +0200

    Add various useful packages, add a release module

--- scripts/R/packages.R
+++ scripts/R/packages.R
@@ -5,14 +5,17 @@
 PACKAGES <- c(
   "box", # Package and module handling
   "box.linters", # Box linting
+  "cli", # Console output
   "covr", # Code coverage
   "devtools", # Package development
+  "fs", # File system handling
   "here", # Correct project document pathing
   "jsonlite", # VSCode interactive terminal
   "languageserver", # Formatting and linting
   "lintr", # Linting and formatting
   "mathjaxr", # Documentation preview
   "pbapply", # Package installation
+  "pkgbuild", # Package building
   "remotes", # Pacakge installation
   "rstudioapi", # RStudio communication
   "tidyverse", # Package installation
