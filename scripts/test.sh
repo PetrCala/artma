b34e9d08bdd46c40d84f670c3bb0f35650d252f2
PetrCala <cala.p@seznam.cz>
2025-04-07 15:05:01 +0200

    Add a standalone test.yaml workflow that runs on PR changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,14 +4,6 @@ set -e
 
 . "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 
-export TESTTHAT=true
-
-TEST_FOLDER_PATH="$PROJECT_ROOT/$TEST_DIR_NAME"
-
-Rscript -e "
-testthat::test_dir("tests")
-"
-
-unset TESTTHAT
+Rscript -e "devtools::test()"
 
 exit 0
