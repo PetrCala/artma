3e86ad10eaef0c1e5249a7a4c025ef4f3b0ca582
PetrCala <cala.p@seznam.cz>
2025-04-07 15:10:38 +0200

    Remove the failing tests

--- tests/testthat/test-dir_create_linter.R
+++ tests/testthat/test-dir_create_linter.R
@@ -1,30 +1,21 @@
-test_that("a placeholder test passes", {
-  expect_true(TRUE)
+ensure_valid_boxpath()
+
+box::use(
+  testthat[test_that],
+  lintr[expect_lint]
+)
+
+test_that("dir_create_linter skips allowed usages", {
+  linter <- dir_create_linter()
+
+  expect_lint("", NULL, linter)
+  expect_lint("fs::dir_create", NULL, linter)
 })
 
-test_that("a test that fails", {
-  expect_true(FALSE)
+test_that("dir_create_linter disallows usage of dir.create()", {
+  linter <- dir_create_linter()
+  lint_msg <- rex::rex("Usage of dir.create() is not allowed.")
+
+  expect_lint("dir.create()", lint_msg, linter)
+  expect_lint("dir.create(file.path('some', 'path.R'))", lint_msg, linter)
 })
-
-
-# ensure_valid_boxpath()
-
-# box::use(
-#   testthat[test_that],
-#   lintr[expect_lint]
-# )
-
-# test_that("dir_create_linter skips allowed usages", {
-#   linter <- dir_create_linter()
-
-#   expect_lint("", NULL, linter)
-#   expect_lint("fs::dir_create", NULL, linter)
-# })
-
-# test_that("dir_create_linter disallows usage of dir.create()", {
-#   linter <- dir_create_linter()
-#   lint_msg <- rex::rex("Usage of dir.create() is not allowed.")
-
-#   expect_lint("dir.create()", lint_msg, linter)
-#   expect_lint("dir.create(file.path('some', 'path.R'))", lint_msg, linter)
-# })
