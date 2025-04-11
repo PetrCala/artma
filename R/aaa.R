f43e930b2a7339376c9f3e15d8dab48369af2e39
PetrCala <cala.p@seznam.cz>
2025-04-11 10:48:27 +0200

    Temporarily disable the runtime logging setup

--- R/aaa.R
+++ R/aaa.R
@@ -12,13 +12,15 @@ runtime_setup <- function(
     FUN,
     options_file_name = NULL,
     options_dir = NULL) {
-  box::use(logs = artma / libs / logs / index)
+  # box::use(logs = artma / libs / logs / index)
 
   runtime_options <- options.load(options_file_name = options_file_name, options_dir = options_dir)
   withr::local_options(runtime_options)
 
-  logs$setup_logging()
-  FUN()
+  message("Loading runtime options from ", options_file_name, " file.")
+
+  # logs$setup_logging()
+  # FUN()
 }
 
 # nolint end: unused_declared_object_linter, box_usage_linter.
