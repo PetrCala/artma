ec1c454b48c82f04db5bbded826db5f6a076edc2
PetrCala <cala.p@seznam.cz>
2025-04-11 10:52:17 +0200

    Add a couple of debug statements

--- R/aaa.R
+++ R/aaa.R
@@ -12,15 +12,13 @@ runtime_setup <- function(
     FUN,
     options_file_name = NULL,
     options_dir = NULL) {
-  # box::use(logs = artma / libs / logs / index)
+  box::use(logs = artma / libs / logs / index)
 
   runtime_options <- options.load(options_file_name = options_file_name, options_dir = options_dir)
   withr::local_options(runtime_options)
 
-  message("Loading runtime options from ", options_file_name, " file.")
-
-  # logs$setup_logging()
-  # FUN()
+  logs$setup_logging()
+  FUN()
 }
 
 # nolint end: unused_declared_object_linter, box_usage_linter.
