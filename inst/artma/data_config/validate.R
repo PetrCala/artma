b3e5b9d136ca4ffd747d8d4659bf3f72c0069ae8
PetrCala <cala.p@seznam.cz>
2025-04-16 15:59:10 +0200

    feat: add a function that validates an existence of a data config file

--- inst/artma/data_config/validate.R
+++ inst/artma/data_config/validate.R
@@ -12,6 +12,31 @@ validate_data_config_filename <- function(filename) {
   grepl(CONST$REGEX$DATA_CONFIG_FILE_SUFFIX, filename)
 }
 
+#' @title Check if data config file exists
+#' @description Check if a data configuration file exists for a given data frame name in the specified directory.
+#' @note Provide the name of the data frame including the suffix.
+#' @param df_name *\[character\]* Name of the data frame to check for a config file
+#' @param data_config_dir *\[character, optional\]* Directory to search for the config file. If `NULL`, uses the default directory.
+#' @return *\[logical\]* `TRUE` if a matching config file exists, `FALSE` otherwise
+df_data_config_exists <- function(df_name, data_config_dir = NULL) {
+  box::use(
+    artma / const[CONST],
+    artma / paths[PATHS]
+  )
+
+  df_name <- tools::file_path_sans_ext(df_name)
+
+  data_config_dir <- if (is.null(data_config_dir)) PATHS$DIR_USR_DATA_CONFIGS else data_config_dir
+  if (!dir.exists(data_config_dir)) {
+    cli::cli_abort("The following data config directory does not exist: {.path {data_config_dir}}")
+  }
+
+  pattern <- paste0("^", df_name, CONST$REGEX$DATA_CONFIG_FILE_SUFFIX, "$")
+  matching_files <- list.files(path = data_config_dir, pattern = pattern, full.names = TRUE)
+  return(length(matching_files) > 0)
+}
+
 box::export(
-  validate_data_config_filename
+  validate_data_config_filename,
+  df_data_config_exists
 )
