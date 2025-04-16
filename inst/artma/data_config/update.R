876c9c335db0e50159eb5af7a9ee8c53e6a2798c
PetrCala <cala.p@seznam.cz>
2025-04-16 17:37:07 +0200

    refactor: rename the data config file update function to better reflect its purpose

--- inst/artma/data_config/update.R
+++ inst/artma/data_config/update.R
@@ -1,10 +1,10 @@
 #' @title Update a data config file
 #' @description Update a data config file with a new dataframe. If the data config file does not exist, it will be created.
-#' @param config *\[list\]* The data config to use, provided as a list.
+#' @param data_config *\[list\]* The data config to use, provided as a list.
 #' @param df_name *\[character\]* The name of the dataframe for which the data config is being updated.
 #' @param data_config_dir *\[character, optional\]* The directory to save the data config. If `NULL`, uses the default directory.
 #' @return *\[NULL\]* The function is called for its side effects
-update_data_config <- function(config, df_name, data_config_dir = NULL) {
+update_data_config_file <- function(data_config, df_name, data_config_dir = NULL) {
   box::use(
     artma / paths[PATHS],
     artma / libs / ask[ask_for_overwrite_permission],
@@ -21,12 +21,12 @@ update_data_config <- function(config, df_name, data_config_dir = NULL) {
   ask_for_overwrite_permission(file_path, action_name = "updating a data config file")
   cli::cli_inform("Writing data config file to {.path {file_path}}")
   jsonlite::write_json(
-    config,
+    data_config,
     file_path
   )
   return(invisible(NULL))
 }
 
 box::export(
-  update_data_config
+  update_data_config_file
 )
