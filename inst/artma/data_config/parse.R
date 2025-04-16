6d524db819c9bceaf414ccc3c66e8e406868d550
PetrCala <cala.p@seznam.cz>
2025-04-16 17:10:18 +0200

    chore: add an optional 'should_validate' argument to data config filename parsing

--- inst/artma/data_config/parse.R
+++ inst/artma/data_config/parse.R
@@ -1,15 +1,16 @@
 #' Construct a data config filename from a dataframe name
 #'
 #' @param df_name *\[character\]* The name of the dataframe
+#' @param should_validate *\[logical\]* Whether to validate the filename
 #' @return *\[character\]* The filename of the data config
-construct_data_config_filename <- function(df_name) {
+construct_data_config_filename <- function(df_name, should_validate = TRUE) {
   box::use(
     artma / const[CONST],
     artma / data_config / validate[data_config_filename_is_valid]
   )
 
   filename <- paste0(df_name, CONST$PATTERNS$DATA_CONFIG$PLAIN)
-  if (!data_config_filename_is_valid(filename)) {
+  if (should_validate && !data_config_filename_is_valid(filename)) {
     cli::cli_abort("The following data config filename is not valid: {.path {filename}}")
   }
   filename
@@ -40,20 +41,22 @@ parse_df_into_data_config <- function(df, df_name, data_config_dir = NULL) {
 
   filename <- construct_data_config_filename(df_name)
 
-  # Create a path to the file
-  file_path <- file.path(data_config_dir, filename)
+  browser()
 
-  # Create the file
-  file.create(file_path)
+  # # Create a path to the file
+  # file_path <- file.path(data_config_dir, filename)
 
-  # Write the dataframe to the file
-  write.csv(df, file_path, row.names = FALSE)
+  # # Create the file
+  # file.create(file_path)
 
-  # Return the data config
-  list(
-    filename = filename,
-    file_path = file_path
-  )
+  # # Write the dataframe to the file
+  # write.csv(df, file_path, row.names = FALSE)
+
+  # # Return the data config
+  # list(
+  #   filename = filename,
+  #   file_path = file_path
+  # )
 }
 
 box::export(
