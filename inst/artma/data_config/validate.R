d78ef394ea00eebf9a343158e562519837a58391
PetrCala <cala.p@seznam.cz>
2025-04-16 15:13:47 +0200

    feat: add a function for validating data config filenames

---
+++ inst/artma/data_config/validate.R
@@ -0,0 +1,12 @@
+#' Validate that a filename follows the data config file naming convention
+#' @description Check if a filename ends with '.config.json'
+#' @param filename *\[character\]* The filename to validate
+#' @return *\[logical\]* `TRUE` if the filename is valid, `FALSE` otherwise
+validate_data_config_filename <- function(filename) {
+  box::use(artma / const[CONST])
+  grepl(CONST$REGEX$DATA_CONFIG_FILE_SUFFIX, filename)
+}
+
+box::export(
+  validate_data_config_filename
+)
