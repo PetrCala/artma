49341f50cb6f29f0bddc6d30be25eafd672ef43f
PetrCala <cala.p@seznam.cz>
2025-04-16 17:36:14 +0200

    refactor: move the data config write function to a standalone module

--- inst/artma/data_config/parse.R
+++ inst/artma/data_config/parse.R
@@ -17,46 +17,16 @@ construct_data_config_filename <- function(df_name, should_validate = TRUE) {
 }
 
 
-#' Write a data config to a file
-#'
-#' @param config *\[list\]* The data config
-#' @param file_path *\[character\]* The path to the file to write the data config to
-#' @return *\[NULL\]* The function is called for its side effects
-write_data_config <- function(config, df_name, data_config_dir = NULL) {
-  box::use(
-    artma / paths[PATHS],
-    artma / libs / ask[ask_for_overwrite_permission],
-    artma / libs / file_utils[ensure_folder_existence]
-  )
-
-  data_config_dir <- if (is.null(data_config_dir)) PATHS$DIR_USR_DATA_CONFIGS else data_config_dir
-  ensure_folder_existence(data_config_dir)
-
-  filename <- construct_data_config_filename(df_name)
-  file_path <- file.path(data_config_dir, filename)
-
-  ask_for_overwrite_permission(file_path, action_name = "updating a data config file")
-  cli::cli_inform("Writing data config file to {.path {file_path}}")
-  jsonlite::write_json(
-    config,
-    file_path
-  )
-}
-
 
 #' Parse a dataframe into a data config
 #'
 #' @param df *\[data.frame\]* The dataframe to parse
 #' @param df_name *\[character\]* The name of the dataframe
 #' @param data_config_dir *\[character\]* The directory to save the data config
-#' @param should_write *\[logical\]* Whether to write the data config to a file
 #' @return *\[list\]* The data config
-parse_df_into_data_config <- function(df, df_name, data_config_dir = NULL, should_write = FALSE) {
+parse_df_into_data_config <- function(df, df_name, data_config_dir = NULL) {
   box::use(
-    artma / const[CONST],
-    artma / paths[PATHS],
-    artma / libs / validation[validate],
-    artma / libs / file_utils[ensure_folder_existence]
+    artma / libs / validation[validate]
   )
 
   validate(
@@ -66,13 +36,10 @@ parse_df_into_data_config <- function(df, df_name, data_config_dir = NULL, shoul
 
   config <- list()
 
-  if (should_write) write_data_config(config, df_name, data_config_dir)
-
   config
 }
 
 box::export(
   construct_data_config_filename,
-  parse_df_into_data_config,
-  write_data_config
+  parse_df_into_data_config
 )
---
+++ inst/artma/data_config/update.R
@@ -0,0 +1,32 @@
+#' @title Update a data config file
+#' @description Update a data config file with a new dataframe. If the data config file does not exist, it will be created.
+#' @param config *\[list\]* The data config to use, provided as a list.
+#' @param df_name *\[character\]* The name of the dataframe for which the data config is being updated.
+#' @param data_config_dir *\[character, optional\]* The directory to save the data config. If `NULL`, uses the default directory.
+#' @return *\[NULL\]* The function is called for its side effects
+update_data_config <- function(config, df_name, data_config_dir = NULL) {
+  box::use(
+    artma / paths[PATHS],
+    artma / libs / ask[ask_for_overwrite_permission],
+    artma / libs / file_utils[ensure_folder_existence],
+    artma / data_config / parse[construct_data_config_filename]
+  )
+
+  data_config_dir <- if (is.null(data_config_dir)) PATHS$DIR_USR_DATA_CONFIGS else data_config_dir
+  ensure_folder_existence(data_config_dir)
+
+  filename <- construct_data_config_filename(df_name)
+  file_path <- file.path(data_config_dir, filename)
+
+  ask_for_overwrite_permission(file_path, action_name = "updating a data config file")
+  cli::cli_inform("Writing data config file to {.path {file_path}}")
+  jsonlite::write_json(
+    config,
+    file_path
+  )
+  return(invisible(NULL))
+}
+
+box::export(
+  update_data_config
+)
