4266df5ee869c38c0c509ec1d68621ce433ecc15
PetrCala <cala.p@seznam.cz>
2025-04-24 16:47:20 +0200

    fix: fix an import error

--- inst/artma/data/index.R
+++ inst/artma/data/index.R
@@ -1,13 +1,15 @@
-box::use(
-  artma / data / read[read_data],
-  artma / data / preprocess[preprocess_data]
-)
-
 #' @title Prepare data
 #' @description Prepare data for analysis. This includes reading, preprocessing, cleaning, and validating the data.
 #' @return *\[data.frame\]* The prepared data frame.
 prepare_data <- function() {
   cli::cli_inform("Preparing data for analysis.")
+
+  box::use(
+    artma / data / read[read_data],
+    artma / data / preprocess[preprocess_data],
+    artma / data / compute[compute_optional_columns]
+  )
+
 
   df <- read_data()
   df <- preprocess_data(df)
