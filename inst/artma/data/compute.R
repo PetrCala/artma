f34b3de06abb84c0cba1584d615bf1e2e7d13449
PetrCala <cala.p@seznam.cz>
2025-04-25 13:39:49 +0200

    feat: add a utility function for calculating study sizes

--- inst/artma/data/compute.R
+++ inst/artma/data/compute.R
@@ -1,19 +1,4 @@
-#' @title Get optional column name
-#' @description Get the user-defined name of an optional column.
-#' @param colname *\[character\]* The standardized name of the column to get.
-#' @return *\[character\]* The user-defined name of the column.
-#' @keywords internal
-get_optional_colname <- function(colname) {
-  usr_colname <- getOption(paste0("artma.data.colnames.", colname))
-
-  if (is.null(usr_colname)) {
-    cli::cli_abort("The column name for the {.val {colname}} is not set.")
-  }
-
-  if (is.na(usr_colname)) usr_colname <- colname # Default to the standardized name
-
-  usr_colname
-}
+box::use(artma / data / utils[get_usr_colname])
 
 #' @title Add observation ID column
 #' @description Add an observation ID column to the data frame.
@@ -21,7 +6,7 @@ get_optional_colname <- function(colname) {
 #' @return *\[data.frame\]* The data frame with the observation ID column.
 #' @keywords internal
 add_obs_id_column <- function(df) {
-  colname <- get_optional_colname("obs_id")
+  colname <- get_usr_colname("obs_id")
 
   if (!colname %in% colnames(df)) {
     df[[colname]] <- seq_len(nrow(df))
@@ -49,7 +34,7 @@ add_obs_id_column <- function(df) {
 add_study_id_column <- function(df) {
   box::use(artma / libs / validation[assert])
 
-  colname <- get_optional_colname("study_id")
+  colname <- get_usr_colname("study_id")
 
   study_colname <- getOption("artma.data.colnames.study")
   study_names <- df[[study_colname]]
@@ -85,7 +70,7 @@ add_t_stat_column <- function(df) {
     artma / const[CONST],
     calc = artma / calc / index
   )
-  colname <- get_optional_colname("t_stat")
+  colname <- get_usr_colname("t_stat")
   opt_path <- "artma.data.colnames.t_stat"
 
   if (colname %in% colnames(df)) {
@@ -109,6 +94,35 @@ add_t_stat_column <- function(df) {
   df
 }
 
+#' @title Add study size column
+#' @description Add a study size column to the data frame.
+#' @param df *\[data.frame\]* The data frame to add the study size column to.
+#' @return *\[data.frame\]* The data frame with the study size column.
+#' @keywords internal
+add_study_size_column <- function(df) {
+  colname <- get_usr_colname("study_size")
+  study_id_colname <- get_usr_colname("study_id")
+
+  study_id_col <- df[[study_id_colname]]
+
+  freq_table <- table(study_id_col)
+  study_size_col <- vapply(study_id_col, function(x) freq_table[as.character(x)], FUN.VALUE = integer(1))
+
+  if (colname %in% colnames(df)) {
+    invalid_or_missing_ids <- which(is.na(df[[colname]]) | df[[colname]] != study_size_col)
+    if (length(invalid_or_missing_ids) > 0) {
+      cli::cli_alert_warning(c(
+        "!" = "Found {length(invalid_or_missing_ids)} invalid or missing study sizes in the column {.val {colname}}.",
+        "i" = "Resetting them to the expected values."
+      ))
+    }
+  }
+
+  df[[colname]] <- study_size_col
+
+  df
+}
+
 
 
 #' @title Compute optional columns
@@ -123,7 +137,8 @@ compute_optional_columns <- function(df) {
   df %>%
     add_obs_id_column() %>%
     add_study_id_column() %>%
-    add_t_stat_column()
+    add_t_stat_column() %>%
+    add_study_size_column()
 }
 
 box::export(compute_optional_columns)
