c4afe07dcc9868331f615d8d415c7b5a92448a88
Petr Čala <cala.p@seznam.cz>
2025-10-01 15:55:22 +0200

    feat: add non-linear publication bias diagnostics

---
+++ inst/artma/calc/methods/maive.R
@@ -0,0 +1,7 @@
+#' @title Placeholder for the MAIVE method
+#' @description Temporary stub until the MAIVE implementation is ported.
+run_maive <- function(...) {
+  cli::cli_abort("The MAIVE method is not yet implemented in the modern pipeline.")
+}
+
+box::export(run_maive)
