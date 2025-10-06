18163c05a6d60b0f7f9b02290f1b784c1aa3a6dc
PetrCala <cala.p@seznam.cz>
2025-10-06 15:08:10 +0200

    fix: udpate the fix_options.R script

--- scripts/R/fix_options.R
+++ scripts/R/fix_options.R
@@ -3,7 +3,7 @@ default_options_file_path <- file.path("inst", "artma", "options", "templates")
 
 cli::cli_inform(glue::glue("Re-creating the default options .yaml file under { default_options_file_path}…"))
 artma::options.create( # nolint: namespace_linter.
-  options_name = "options_default",
+  options_file_name = "options_default",
   options_dir = default_options_file_path
 )
 cli::cli_inform("Done.")
