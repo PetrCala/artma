d83be303c909af6dc791c666238eb4377856fedf
PetrCala <cala.p@seznam.cz>
2026-02-13 15:30:56 +0100

    chore: update the test example file

--- scripts/R/test_example.R
+++ scripts/R/test_example.R
@@ -18,36 +18,10 @@ test <- function() {
   #   file_path = mock_df_path
   # )
 
-  # Artma methods
-  options_file_name <- CONST$MOCKS$TMP_OPTIONS_FILE_NAME
-
-  withr::local_options(list("artma.verbose" = 3))
-  artma::options.delete(options_file_name = options_file_name, skip_confirmation = TRUE)
-
-  # New workflow: much simpler column specification
-  # The package will now automatically recognize columns and only prompt for missing ones
-  artma::options.create(
-    options_file_name = options_file_name,
-    user_input = list(
-      "verbose" = get_verbosity(),
-      "cache.use_cache" = FALSE, # Disable cache to see progress bars
-      "data.source_path" = mock_df_path,
-      "data.winsorization_level" = 0.1,
-      "data.na_handling" = "mice",
-      "data.config_setup" = "auto" # "auto" will recognize columns automatically, "manual" for interactive
-      # Column mappings will be auto-detected - no need to specify them manually!
-      # If auto-detection fails, the package will prompt interactively
-    ),
-    should_overwrite = TRUE
+  artma::artma(
+    # options = "bachelor-thesis.yaml",
+    # methods = c("variable_summary_stats", "p_hacking_tests")
   )
-
-  # artma::artma(
-  #   options = options_file_name,
-  #   FUN = function() {
-  #     box::use(artma / data_config / read[get_data_config])
-  #     config <- get_data_config(fix_if_invalid = TRUE)
-  #   }
-  # )
 }
 
 test()
